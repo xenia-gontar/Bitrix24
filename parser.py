8371a07d9ceae333559a74457097235fcf7069b6
Xenia Gontar <118064189+xenia-gontar@users.noreply.github.com>
2024-07-29 17:22:04 +0300

    Update parser.py

--- parser.py
+++ parser.py
@@ -4,4 +4,6 @@ from bs4 import BeautifulSoup
 page = open("index.html", "r+")
 soup = BeautifulSoup(page, "html.parser")
 
-print(soup.find_all('dd',itemprop="foundingDate"))
+result = soup.find_all('dd',itemprop="foundingDate")
+
+print(result[0].get_text())
