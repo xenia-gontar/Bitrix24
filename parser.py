ef3c8c2f967593374dc4f965e028cca92e135e1f
Xenia Gontar <118064189+xenia-gontar@users.noreply.github.com>
2024-07-30 12:29:22 +0300

    Update parser.py

--- parser.py
+++ parser.py
@@ -1,9 +1,32 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from webdriver_manager.chrome import ChromeDriverManager
 from bs4 import BeautifulSoup
 
+options = Options()
+# options.add_argument('--headless')
+# options.add_argument('--no-sandbox')
+options.add_argument('--disable-dev-shm-usage')
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 
-page = open("index.html", "r+")
-soup = BeautifulSoup(page, "html.parser")
 
-result = soup.find_all('dd',itemprop="foundingDate")
+driver.get('')
+
+time.sleep(4)
+search = driver.find_element(By.CLASS_NAME,"index-search-input")
+search.send_keys("7707049388")
+search.send_keys(Keys.RETURN)
+
+time.sleep(5)
+
+content = driver.page_source
+page = BeautifulSoup(content, "html.parser")
+
+result = page.find_all('dd',itemprop="foundingDate")
 
 print(result[0].get_text())
+driver.close()
